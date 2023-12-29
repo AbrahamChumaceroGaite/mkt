8dcffb8725d5c7c3fdc3530bc441fbff1def2547
AbrahamChumaceroGaite <abraham.chumacero@ucb.edu.bo>
2023-12-29 17:04:29 -0400

    prealpha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 
 export const routes: Routes = [
-
   {
-      path: '', component: NavComponent, children: [
-          {
-              path: '', loadChildren: () => import('./core/client-side/client-side.module').then(m => m.ClientSideModule)
-          },
-          {
-              path: '', loadChildren: () => import('./core/admin-side/admin-side.module').then(m => m.AdminSideModule)
-          }
-      ]
+    path: '', redirectTo: '/lobby', pathMatch: 'full'
   },
-
-  { path: '', redirectTo: 'lobby', pathMatch: 'full' },
-  
-  { path: '**', redirectTo: 'lobby' },
-
+  {
+    path: '', component: NavComponent, children: [
+      {
+        path: '', loadChildren: () => import('./core/client-side/client-side.module').then(m => m.ClientSideModule)
+      },
+      {
+        path: '', loadChildren: () => import('./core/admin-side/admin-side.module').then(m => m.AdminSideModule)
+      }
+    ]
+  },
+  {
+    path: '**', redirectTo: '/lobby'
+  },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
