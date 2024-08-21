20e3f1e0655fd8a1af6f21945e71070ff53375c6
AbrahamChumaceroGaite <abraham.chumacero@ucb.edu.bo>
2024-08-21 01:57:31 -0400

    presentacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.mkt.dev.dtt.tja.ucb.edu.bo/mkt/api',
-  apiSocket: 'https://api.mkt.dev.dtt.tja.ucb.edu.bo'
-/*   apiUrl: 'http://localhost:80/mkt/api',
-  apiSocket: 'http://localhost:80' */
+/*   apiUrl: 'https://api.mkt.dev.dtt.tja.ucb.edu.bo/mkt/api',
+  apiSocket: 'https://api.mkt.dev.dtt.tja.ucb.edu.bo' */
+  apiUrl: 'http://localhost:80/mkt/api',
+  apiSocket: 'http://localhost:80'
 }
